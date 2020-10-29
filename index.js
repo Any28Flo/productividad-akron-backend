4d9a475c5652800f5a3a16457043245a18449e1d
Daedra <foglzerika@gmail.com>
2020-10-29 04:45:08 -0600

    feat|creating router

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require ('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const initDB = require("./services/mongodb");
+const taskRoutes = require('./middlewares/taskRoutes')
+const PORT = 8080 || process.env.PORT;
 
 const app = express();
 
-const PORT = 8080 || process.env.PORT;
+app.use(cors({
+    credentials : true,
+    origin: ['http://localhost:3000']
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 initDB()
+
+app.use('/task', taskRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 app running on ${PORT}`)
