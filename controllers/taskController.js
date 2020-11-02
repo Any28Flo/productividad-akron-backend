6d04045cc725aaeb28abd7c6e5bf18491b58791d
Daedra <foglzerika@gmail.com>
2020-11-02 09:24:59 -0600

    feat| Delete task endopoint

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,5 @@
 const Task = require('../models/taskSchema');
+const { validationResult } = require('express-validator');
 
 exports.registerTask = async (req, res) =>{
     const {taskName, description, status, duration, user} = req.body;
@@ -28,7 +29,7 @@ exports.registerTask = async (req, res) =>{
 }
 exports.listTasks = async(req, res) =>{
     const {user} = req.headers;
-    const tasks = await Task.find({user: user});
+    const tasks = await Task.find({user: user, status: 'active' });
     if (tasks !== null){
         return (
             res.status(200).json({
@@ -39,4 +40,67 @@ exports.listTasks = async(req, res) =>{
     }
 
 
+}
+exports.detailTask = async (req, res) =>{
+    const {idTask} = req.params;
+
+    try{
+        const task = await Task.findOne({_id: idTask});
+        if(task){
+            return(
+                res.status(200).json({
+                    task
+                })
+            )
+        }
+    }catch (e) {
+     console.log(2)
+    }
+}
+exports.editTask = async(req,res) =>{
+    const errors= validationResult(res);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() })
+    }
+
+    const {taskName, description, status, duration, user} = req.body;
+    const editTask = {
+        taskName: taskName,
+        description: description,
+        status: status,
+        duration: duration,
+        user: user
+    }
+    try{
+        const editTaskObj = await Task.findByIdAndUpdate(
+            { _id: req.params.idTask },
+            { $set: editTask },
+            { new: true }
+        );
+        if(editTaskObj){
+            return (
+                res.status(200).json({
+                    msg: `${editTaskObj.taskName} editado exitosamente`,
+                    editTaskObj
+                })
+            )
+        }
+    }catch (e) {
+
+    }
+}
+exports.deleteTask = async(req, res) =>{
+    const {idTask} = req.params;
+    const update = {status: 'disabled'};
+    try{
+        const deleteTask = await Task.findByIdAndUpdate(idTask, update)
+        if(deleteTask){
+            res.status(200).json({
+                msg : "Task eliminada correctamente"
+            })
+
+        }
+    }catch (e) {
+
+    }
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 const taskController = require('./../controllers/taskController');
+const { check } = require( 'express-validator');
 
 routes.post('/', taskController.registerTask);
 routes.get('/', taskController.listTasks);
+routes.get('/:idTask',
+    [
+        check('description', 'La descripción es requerida').not().isEmpty(),
+        check('duration', 'La duración es requerida').not().isEmpty()
+    ],
+    taskController.detailTask);
+routes.put('/:idTask', taskController.editTask)
+routes.put ('/deleteTask/:idTask', taskController.deleteTask)
 module.exports = routes;
